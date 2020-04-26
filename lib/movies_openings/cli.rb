858e8f03d3f3fb20703f9925cd6603238e6099f0
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2020-04-26 20:39:02 +0000

    cli.rb

--- lib/movies_openings/cli.rb
+++ lib/movies_openings/cli.rb
@@ -22,7 +22,7 @@ class MoviesOpenings::CLI
       
       if input.to_i > 0
         movie = @movies[input.to_i-1]
-        puts "#{movie.name} - #{movie.plot}"
+        puts "#{movie.name}"
       elsif input == "list"
         list_movies
       else
